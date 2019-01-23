f00aebf74aeb02ee496ffe4ad2fc03546b43fe92
Danielle Ismerim <danismerim@gmail.com>
2019-01-23 15:50:11 +0100

    forcing js update

--- extension.driver.php
+++ extension.driver.php
@@ -25,7 +25,7 @@ Class extension_tag_ui_selector extends Extension
         $callback = Symphony::Engine()->getPageCallback();
 
         if ($callback['driver'] == 'publish' && $callback['context']['page'] !== 'index') {
-            Administration::instance()->Page->addScriptToHead(URL . '/extensions/tag_ui_selector/assets/tagui.selector.publish.js');
+            Administration::instance()->Page->addScriptToHead(URL . '/extensions/tag_ui_selector/assets/tagui.selector.publish.js?1');
             Administration::instance()->Page->addStylesheetToHead(URL . '/extensions/tag_ui_selector/assets/tagui.selector.publish.css');
         }
     }
